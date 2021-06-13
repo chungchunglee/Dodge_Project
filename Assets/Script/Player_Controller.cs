df82580f04a3a645736f917a39ab106c6f754035
Chung Heon Lee <55828069+chungchunglee@users.noreply.github.com>
2021-06-14 00:11:48 +0900

    Player Control 기능 변경 - Addfoce -> Vector velocity 직접 변경
    관성 x 값 변경

--- Assets/Script/Player_Controller.cs
+++ Assets/Script/Player_Controller.cs
@@ -15,6 +15,7 @@ public class Player_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /* This is first Controll Code
         if(Input.GetKey(KeyCode.UpArrow)==true)
         {
             playerRigidbody.AddForce(0f, 0f, speed);
@@ -31,6 +32,16 @@ public class Player_Controller : MonoBehaviour
         {
             playerRigidbody.AddForce(-speed, 0f, 0f);
         }
+        */
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+        playerRigidbody.velocity = newVelocity;
+
     }
     public void Die()
     {
